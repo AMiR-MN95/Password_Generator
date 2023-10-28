9fe4b9ac745fa7ba8bc81b7f75acb80f7a349ce8
AMiR <Amir_MostafaviNejad@yahoo.com>
2023-10-28 18:42:22 +0330

    "Add Git ignore, remove rand.Seed & get password length"

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
+	"regexp"
 )
 
 const (
@@ -17,8 +17,6 @@ func generatePassword(length int) string {
 	allChars := lowerChars + upperChars + numberChars + specialChars
 	password := make([]byte, length)
 
-	rand.Seed(time.Now().UnixNano())
-
 	for i := range password {
 		password[i] = allChars[rand.Intn(len(allChars))]
 	}
@@ -26,7 +24,35 @@ func generatePassword(length int) string {
 	return string(password)
 }
 
+func passwordMeetsCriteria(password string) bool {
+	// Define regular expressions to match criteria
+	containsLower := regexp.MustCompile(`[a-z]`)
+	containsUpper := regexp.MustCompile(`[A-Z]`)
+	containsNumber := regexp.MustCompile(`[0-9]`)
+	containsSpecial := regexp.MustCompile(`[!@#%^&*()-_=+\[\]{}|;:'",.<>/?]`)
+
+	return containsLower.MatchString(password) &&
+		containsUpper.MatchString(password) &&
+		containsNumber.MatchString(password) &&
+		containsSpecial.MatchString(password)
+}
+
 func main() {
-	password := generatePassword(8)
+	var length int
+	fmt.Print("Enter the desired password length: ")
+	_, err := fmt.Scanf("%d", &length)
+	if err != nil {
+		fmt.Println("Invalid input. Please enter a valid integer.")
+		return
+	}
+
+	password := generatePassword(length)
+
 	fmt.Println("Generated Password:", password)
+
+	if passwordMeetsCriteria(password) {
+		fmt.Println("Password meets criteria.")
+	} else {
+		fmt.Println("Password does not meet criteria.")
+	}
 }
